793cfb9887d2ec85a8e5e2e4df9ef2256f782113
paolomococci <paolomococci@gmail.com>
2024-01-20 16:15:20 +0100

    standardized function name format

--- tools/latest_three_mac_gen.py
+++ tools/latest_three_mac_gen.py
@@ -3,8 +3,8 @@
 import random
 
 # generate latest three digit string of dummy MAC address
-def latestThreePseudoRandomOfDummyMAC():
+def latest_three_pseudo_random_of_dummy_mac():
         latest_three_mac = [ random.randint(0x00, 0x7f), random.randint(0x00, 0xff), random.randint(0x00, 0xff) ]
         return ':'.join(map(lambda x: "%02x" % x, latest_three_mac))
 
-print(latestThreePseudoRandomOfDummyMAC())
+print(latest_three_pseudo_random_of_dummy_mac())
