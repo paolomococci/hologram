ac94bfd90e329f77ec56ca25b7ed418d98a9a2e8
paolomococci <paolomococci@gmail.com>
2024-03-10 11:25:40 +0100

    correction of an oversight

--- examples/quotes/quotes/resources/views/components/authentication-card-logo.blade.php
+++ examples/quotes/quotes/resources/views/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
-<div class="justify-center w-12 h-20 rounded-3xl bg-red-50 dark:bg-red-800/20">
+<div class="justify-center w-20 h-20 rounded bg-red-50 dark:bg-red-800/20">
     <a href="/" title="come back to the welcome page">
         <i class="bi bi-house icon-fuchsia-3"></i>
     </a>
