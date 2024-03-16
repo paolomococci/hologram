7589aeecaec992f32bd933e9f1206a1a8657d652
paolomococci <paolomococci@gmail.com>
2024-03-16 10:52:35 +0100

    correction of an oversight

--- examples/quotes/quotes/public/info.php
+++ examples/quotes/quotes/public/info.php
@@ -1,17 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP info</title>
-    <style>
-    </style>
-</head>
-<body>
-    <header>
-    </header>
-    <main>
-        <? phpinfo(INFO_ALL); ?>
-    </main>
-</body>
-</html>
+<? phpinfo(INFO_ALL);
