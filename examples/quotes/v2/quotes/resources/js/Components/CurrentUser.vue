a8d1f58099c5e05eec239d06f8b293859fcab2df
paolomococci <paolomococci@gmail.com>
2024-03-28 16:27:59 +0100

    useful component for viewing the current user's data

---
+++ examples/quotes/v2/quotes/resources/js/Components/CurrentUser.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <div class="mt-4 text-sm leading-relaxed text-gray-500">
+        <ul>
+            <li>ID: {{ page.props.auth.user.id }}</li>
+            <li>Name: {{ page.props.auth.user.name }}</li>
+            <li>Email: {{ page.props.auth.user.email }}</li>
+            <li>Registered on: {{ page.props.auth.user.created_at }}</li>
+        </ul>
+    </div>
+
+</template>
+
+<script setup>
+import { usePage } from '@inertiajs/vue3'
+
+const page = usePage()
+</script>
