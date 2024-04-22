3fc590c28fb43bc8918e05643368dfca3f7126b9
paolomococci <paolomococci@gmail.com>
2024-04-22 18:52:45 +0200

    encountering a problem: undefined method User::ownedTeams()

--- examples/quotes/v3/quotes/app/Models/User.php
+++ examples/quotes/v3/quotes/app/Models/User.php
@@ -6,10 +6,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasTeams;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use HasTeams;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
