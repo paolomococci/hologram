d2512aacffb005e5b985225fb83ed061ff9c08ba
paolomococci <paolomococci@gmail.com>
2024-05-07 19:06:28 +0200

    I cleaned up the code

--- examples/quotes/v3/quotes/routes/web.php
+++ examples/quotes/v3/quotes/routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Inertia\Inertia;
-use App\Models\Article;
-use App\Utils\SanitizerUtil;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Application;
 use App\Http\Controllers\ToolController;
