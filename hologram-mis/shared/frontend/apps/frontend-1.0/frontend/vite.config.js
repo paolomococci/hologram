6b5482fd1ac76d287d260dce04f1dce0dac0b17b
paolomococci <paolomococci@gmail.com>
2024-09-06 08:49:08 +0200

    to facilitate development

--- hologram-mis/shared/frontend/apps/frontend-1.0/frontend/vite.config.js
+++ hologram-mis/shared/frontend/apps/frontend-1.0/frontend/vite.config.js
@@ -1,20 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url"
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueDevTools from "vite-plugin-vue-devtools"
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   server: {
     host: true,
-    port: 8080
+    port: 8080,
   },
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  define: {
+    __VUE_PROD_DEVTOOLS__: mode !== "production",
+  },
+}))
