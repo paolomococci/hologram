9157523347d2a63d47886b80b7a54de7fecb17f6
paolomococci <paolomococci@gmail.com>
2024-09-19 12:29:08 +0200

    correcting an oversight

--- hologram-kind/docker-playground/hologram-7.0/dev/landing/source/index.php
+++ hologram-kind/docker-playground/hologram-7.0/dev/landing/source/index.php
@@ -3,18 +3,12 @@
 
 <head>
   <meta charset="UTF-8" />
-  <link rel="icon" type="image/png" href="/hologram.png" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Hologram 7.0</title>
-  <script type="module" crossorigin src="/assets/index-Bj4XG5fw.js"></script>
-  <link rel="stylesheet" crossorigin href="/assets/index-CTSeDo_N.css">
 </head>
 
 <body>
-  <div id="root"></div>
-  <section>
-    <?php phpinfo(); ?>
-  </section>
+  <?php phpinfo(); ?>
 </body>
 
 </html>
