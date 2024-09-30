c974f9726724af65cad1ef1a8577e5fa02502c1f
paolomococci <paolomococci@gmail.com>
2024-09-30 04:22:06 +0200

    correcting an oversight

--- hologram-caas/templates/backend/lamp-8.3.12/scripts/start.sh
+++ hologram-caas/templates/backend/lamp-8.3.12/scripts/start.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
-php-fpm --nodaemonize --allow-to-run-as-root --fpm-config /opt/php/8.3.11/etc/php-fpm.conf &
+php-fpm --nodaemonize --allow-to-run-as-root --fpm-config /opt/php/8.3.12/etc/php-fpm.conf &
 apache2ctl -D FOREGROUND
