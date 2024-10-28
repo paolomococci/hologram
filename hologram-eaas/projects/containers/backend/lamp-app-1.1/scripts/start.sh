cd21113310c71fec3d58944adbb880779b729776
paolomococci <paolomococci@gmail.com>
2024-10-29 00:46:37 +0700

    upgrade

---
+++ hologram-eaas/projects/containers/backend/lamp-app-1.1/scripts/start.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# setup
+./setup.sh
+
+# start services
+/usr/sbin/sshd -D &
+php-fpm --nodaemonize --allow-to-run-as-root --fpm-config /opt/php/8.3.13/etc/php-fpm.conf &
+apache2ctl -D FOREGROUND
