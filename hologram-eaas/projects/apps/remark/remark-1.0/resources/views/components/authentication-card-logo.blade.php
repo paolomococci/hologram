9be691db488a153b53980b7f70fe63a0b1aec6a2
paolomococci <paolomococci@gmail.com>
2024-12-05 14:57:12 +0100

    a small change

--- hologram-eaas/projects/apps/remark/remark-1.0/resources/views/components/authentication-card-logo.blade.php
+++ hologram-eaas/projects/apps/remark/remark-1.0/resources/views/components/authentication-card-logo.blade.php
@@ -1,8 +1,8 @@
 <a href="/">
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
         stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-        class="lucide lucide-log-in size-16 text-indigo-500">
-        <title>to return to the home page</title>
+        class="text-indigo-500 lucide lucide-log-in size-16">
+        <title>come back to landing view</title>
         <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
         <polyline points="10 17 15 12 10 7" />
         <line x1="15" x2="3" y1="12" y2="12" />
