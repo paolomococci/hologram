a51e63123ed9ec57274839750520d6c8d8a570d6
paolomococci <paolomococci@gmail.com>
2024-12-26 19:16:47 +0100

    improved user experience with url parameters

--- examples/thesis/app/Livewire/View/Search.php
+++ examples/thesis/app/Livewire/View/Search.php
@@ -6,37 +6,37 @@ use App\Models\Article;
 use Livewire\Component;
 use Livewire\Attributes\On;
 use Livewire\Attributes\Lazy;
-use Livewire\Attributes\Validate;
 
 #[Lazy]
 class Search extends Component
 {
-    #[Validate('required')]
     public $searchText = '';
 
-    public $results = [];
-
     public $placeholder = '';
-
-    public function updatedSearchText($searchText)
-    {
-        $this->reset('results');
-        $this->validate();
-        $searchTerm = "%{$searchText}%";
-        $this->results = Article::where('title', 'LIKE', $searchTerm)->get();
-    }
 
     #[On('search:clear-results')]
     public function clear()
     {
         $this->reset(
-            'results',
             'searchText',
         );
     }
 
+    protected function queryString()
+    {
+        return [
+            'searchText' => [
+                'as' => 'q',
+                'history' => true,
+                'except' => '',
+            ],
+        ];
+    }
+
     public function render()
     {
-        return view('livewire.view.search');
+        return view('livewire.view.search', [
+            'results' => Article::where('title', 'LIKE', "%{$this->searchText}%")->get(),
+        ]);
     }
 }
