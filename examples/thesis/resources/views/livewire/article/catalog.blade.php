08d33454a43c288ee617451e49ab5a5030b2c7f5
paolomococci <paolomococci@gmail.com>
2024-12-27 10:04:59 +0100

    refactoring of the actual view

--- examples/thesis/resources/views/livewire/article/catalog.blade.php
+++ examples/thesis/resources/views/livewire/article/catalog.blade.php
@@ -1,8 +1,12 @@
 {{-- refactoring --}}
-<div class="w-2/5 h-full">
-    {{-- useful paragraph during testing TODO: modify the constant TEST_PHASE accordingly --}}
-    <div class="text-xs text-gray-700 dark:text-gray-300">
-        @if (self::TEST_PHASE)
+@php
+    use App\Utils\CleaningUtility;
+@endphp
+
+<div>
+    {{-- useful divide during testing TODO: modify the constant TEST_PHASE accordingly --}}
+    @if (self::TEST_PHASE)
+        <div class="w-2/5 h-full text-xs text-gray-700 dark:text-gray-300">
             <h3>livewire:article.catalog</h3>
             <h5>dispatched variables:</h5>
             <h6>$articleToggle:</h6>
@@ -17,18 +21,16 @@
             <h6>$this->approvedArticles:</h6>
             <p>{{ var_dump($this->approvedArticles) }}</p>
             <h6>$this->deprecatedArticles:</h6>
-            <p>{{ var_dump($this->deprecatedArticles )}}</p>
+            <p>{{ var_dump($this->deprecatedArticles) }}</p>
             <h6>$this->totalNumberOfRetrievedArticles:</h6>
             <p>{{ var_dump($this->totalNumberOfRetrievedArticles) }}</p>
             <h6>$this->totalNumberOfApprovedArticles:</h6>
             <p>{{ var_dump($this->totalNumberOfApprovedArticles) }}</p>
             <h6>$this->totalNumberOfDeprecatedArticles:</h6>
             <p>{{ var_dump($this->totalNumberOfDeprecatedArticles) }}</p>
-        @endif
-    </div>
+        </div>
+    {{-- follows the actual view --}}
+    @elseif (!self::TEST_PHASE)
+        <p>livewire:article.catalog</p>
+    @endif
 </div>
-
-{{-- follows the actual view --}}
-@php
-    use App\Utils\CleaningUtility;
-@endphp
