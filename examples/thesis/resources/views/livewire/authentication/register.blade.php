c6e83685ba162df10bb51dd10982075d5e07dcd9
paolomococci <paolomococci@gmail.com>
2025-01-06 11:01:11 +0100

    view in development

--- examples/thesis/resources/views/livewire/authentication/register.blade.php
+++ examples/thesis/resources/views/livewire/authentication/register.blade.php
@@ -7,6 +7,7 @@
         <p class="text-sm font-light text-slate-600 dark:text-slate-300">
             Nice to meet you! Enter your details to sig up.
         </p>
+        <x-samp.show-errors />
         <form method="POST" wire:submit="registerAnApplicant" class="mt-4 mb-2 w-80 max-w-screen-lg sm:w-96">
             @csrf
             <div class="flex flex-col gap-6 mb-1">
@@ -33,11 +34,6 @@
                         class="px-3 py-2 w-full text-sm bg-transparent rounded-md border border-orange-600 shadow-sm transition duration-300 dark:border-orange-300 placeholder:text-slate-400 text-slate-700 dark:text-slate-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 focus:shadow"
                         placeholder="Your Email" />
                 </div>
-                <div>
-                    @error('email')
-                        <span class="text-red-600">{{ $registerMessage }}</span>
-                    @enderror
-                </div>
                 {{-- <x-input.input-password wire:model="password" /> --}}
                 <div class="w-full max-w-sm min-w-[100px]">
                     <label class="block mb-2 text-sm text-orange-600 dark:text-orange-300">
@@ -47,19 +43,12 @@
                         class="px-3 py-2 w-full text-sm bg-transparent rounded-md border border-orange-600 shadow-sm transition duration-300 dark:border-orange-300 placeholder:text-slate-400 text-slate-700 dark:text-slate-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 focus:shadow"
                         placeholder="Your Password" />
                 </div>
-                <div>
-                    @error('password')
-                        <span class="text-red-600">{{ $registerMessage }}</span>
-                    @enderror
-                </div>
             </div>
             <div class="inline-flex items-center my-2">
                 {{-- <x-checkbox.checkbox-remember-me wire:model="rememberMe" /> --}}
-                <label class="mr-4 mb-3 text-sm cursor-pointer text-slate-600 dark:text-slate-300" for="remember-me">
-                    <input id="remember-me" type="checkbox" value="bulletin_board" wire:model="rememberMe"
-                        class="w-5 h-5 rounded border shadow transition-all appearance-none cursor-pointer peer hover:shadow-md border-slate-200 checked:border-slate-800 checked:bg-green-300 checked:dark:bg-green-600">
-                    Remember Me
-                </label>
+                <input id="rememberMe" type="checkbox" value="bulletin_board" wire:model="rememberMe"
+                    class="w-5 h-5 rounded border shadow transition-all appearance-none cursor-pointer peer hover:shadow-md border-slate-200 checked:border-slate-800 checked:bg-green-300 checked:dark:bg-green-600">
+                <x-label.input-label for="rememberMe" value="{{ __('Remember Me') }}" class="ml-2" />
             </div>
             <div class="w-full max-w-sm min-w-[100px]">
                 <x-button.button-registration />
