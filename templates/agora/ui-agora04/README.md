efdb5e0964b4501dea01c6acc062cfecd3796a4f
paolomococci <paolomococci@gmail.com>
2025-02-28 16:26:47 +0100

    I aligned this declination of the application with the others

--- templates/agora/ui-agora04/README.md
+++ templates/agora/ui-agora04/README.md
@@ -1,5 +1,11 @@
 # `ui-agora04` a legacy example of web application
 
-This is a frontend web application developed using jQuery and Bootstrap frameworks written in JavaScript programming language.
+This is a frontend web application developed using jQuery and Bootstrap frameworks written in JavaScript programming language
 
-![ui-agora03](./screenshots/screenshot_ui-agora04_mobile_first.png)
+Screenshot while waiting for response from API:
+
+![ui-agora04](./screenshots/screenshot_ui-agora04_mobile_first_loading.png)
+
+Screenshot obtained after receiving the response from the API:
+
+![ui-agora04](./screenshots/screenshot_ui-agora04_mobile_first_loaded.png)
--- templates/agora/ui-agora04/public/js/main.js
+++ templates/agora/ui-agora04/public/js/main.js
@@ -1,9 +1,34 @@
-let count = 1
+const baseUrl = "https://api-agora01.hologram-srv.local/";
+let count = 1;
 
 $(document).ready(function () {
+    // increment
     $('#increment').click(
-        function() {
+        function () {
             $('#increment').text(`count is ${count++}`)
         }
-    )
-})
+    );
+
+    // GET sanctum/csrf-cookie
+    $.get(
+        baseUrl + "sanctum/csrf-cookie",
+        function (data) {
+            console.log(`Csrf-cookie: ${data}`);
+        }
+    );
+
+    // GET api/ping
+    $.get(
+        baseUrl + "api/ping",
+        function (data) {
+            console.log('Success:', data);
+            const response = JSON.parse(data);
+            if (response.status === 200) {
+                $("#loading").remove();
+                $("#message").text(response.message);
+            } else {
+                $("#message").text(`Sorry, there was an error. <em>>response.status</em> is: ${response.status}`);
+            }
+        }
+    );
+});
