032bef9a2b81783a659f1ce3e0f384162945e66a
paolomococci <paolomococci@gmail.com>
2025-02-28 10:31:39 +0100

    improvement

--- templates/agora/ui-agora05/src/ping.js
+++ templates/agora/ui-agora05/src/ping.js
@@ -6,15 +6,12 @@ export async function setupPing(
 ) {
 
     // retrieve sanctum/csrf-cookie
-    // let jsonResCookie = null
     const responseCookie = await fetch(uriCookie)
     if (!responseCookie.ok) {
         throw new Error(`Response status: ${responseCookie.status}`)
-        // console.error(`Response status: ${responseCookie.status}`)
     } else {
         try {
-            // jsonResCookie = await responseCookie.json()
-            // console.log(jsonResCookie)
+            console.log('Response of csrf-cookie: ', responseCookie)
         } catch (error) {
             console.error(error)
         }
@@ -25,7 +22,6 @@ export async function setupPing(
     const responseApi = await fetch(uriApi)
     if (!responseApi.ok) {
         throw new Error(`Response status: ${responseApi.status}`)
-        // console.error(`Response status: ${responseApi.status}`)
     } else {
         try {
             jsonResApi = await responseApi.json()
