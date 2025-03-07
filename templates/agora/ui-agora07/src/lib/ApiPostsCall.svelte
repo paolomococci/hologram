6814ea262a0a6933f031a2de4480064b578f047c
paolomococci <paolomococci@gmail.com>
2025-03-07 08:13:33 +0100

    I added a couple of components

---
+++ templates/agora/ui-agora07/src/lib/ApiPostsCall.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  // TODO
+</script>
+
+<main>
+  <div>
+    <!-- TODO -->
+  </div>
+</main>
---
+++ templates/agora/ui-agora07/src/lib/ApiSanctumCsrfCookieCall.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import axios from "axios"
+  import ENV from "../env"
+
+  async function retrieveSanctumCsrfCookie() {
+    try {
+      const response = await axios.get(`${ENV.baseUrl}sanctum/csrf-cookie`)
+      console.log(response)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  retrieveSanctumCsrfCookie()
+</script>
