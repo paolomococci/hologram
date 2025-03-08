e16c93c771a2e66e0cb67f21b34cc7418f48c085
paolomococci <paolomococci@gmail.com>
2025-03-08 11:01:34 +0100

    I removed the padding around the header

--- templates/agora/ui-agora07/src/App.svelte
+++ templates/agora/ui-agora07/src/App.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <main>
-  <header class="container sticky top-0 z-40 shadow-xl rounded-md max-w-dvw bg-[242424]/95 backdrop-blur-sm">
+  <header class="container sticky top-0 z-40 shadow-xl rounded-md max-w-dvw bg-[242424]/95 backdrop-blur-sm mx-0 px-0">
     <div class="flex justify-center items-center">
       <h3 class="m-4 font-thin text-cyan-400 uppercase">Agora</h3>
     </div>
