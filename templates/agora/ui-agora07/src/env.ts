46ea69ce87527fb67463d2e8e2854f6e97c3afc8
paolomococci <paolomococci@gmail.com>
2025-03-13 13:29:17 +0100

    in the view I assigned a number from one to ten to each card containing the post

--- templates/agora/ui-agora07/src/env.ts
+++ templates/agora/ui-agora07/src/env.ts
@@ -1,5 +1,6 @@
 const ENV = {
-    baseUrl: "https://api-agora01.hologram-srv.local/"
+    baseUrl: "https://api-agora01.hologram-srv.local/",
+    cardUriBase: "https://api-agora01.hologram-srv.local/api/posts/"
 }
 
 export default ENV
--- templates/agora/ui-agora07/src/lib/components/PostView.svelte
+++ templates/agora/ui-agora07/src/lib/components/PostView.svelte
@@ -1,25 +1,35 @@
 <script lang="ts">
+  import type Post from "../apis/api-post"
+  import ENV from "../../env"
+
   let { posts } = $props()
 </script>
 
+{#snippet postCard(post: Post, index?: number)}
+  <div class="py-2 my-2 rounded-lg bg-stone-800">
+    <h5
+      class="m-1 font-serif text-xs text-cyan-600 sm:text-sm md:text-base lg:text-lg"
+    >
+      <span class="block mr-1 font-sans text-xl font-light text-cyan-200"
+        >{index}</span
+      >
+      <a href="{ENV.cardUriBase}{post.id.toString()}">{post.title}</a>
+    </h5>
+    <p
+      class="m-1 font-serif text-xs text-justify sm:text-sm md:text-base lg:text-lg line-clamp-2 text-stone-400"
+    >
+      {post.content}
+    </p>
+    <h6 class="m-1 font-mono text-xs text-cyan-400">
+      {post.user.name}
+    </h6>
+  </div>
+{/snippet}
+
 <main>
   <div class="gap-8 items-center">
-    {#each posts as post}
-      <div class="py-2 my-2 rounded-lg bg-stone-800">
-        <h5
-          class="m-1 font-serif text-xs text-cyan-600 sm:text-sm md:text-base lg:text-lg"
-        >
-          <a href="https://api-agora01.hologram-srv.local/api/posts/{post.id}">{post.title}</a>
-        </h5>
-        <p
-          class="m-1 font-serif text-xs text-justify sm:text-sm md:text-base lg:text-lg line-clamp-2 text-stone-400"
-        >
-          {post.content}
-        </p>
-        <h6 class="m-1 font-mono text-xs text-cyan-400">
-          {post.user.name}
-        </h6>
-      </div>
+    {#each posts as post, index}
+      {@render postCard(post, index + 1)}
     {/each}
   </div>
 </main>
